73e5d8bd5d8c844c6535240d33ff85b7a8ab9145
Enzo Avagliano <enzo.avagliano@ynov.com>
2022-11-22 14:47:24 +0100

    Added get day name by day id

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ export const toSnakeCase = (str: string) => str.replace(/(?:^\w|[A-Z]|\b\w)/g, (
 export const toKebabCase = (str: string) => str.replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => (index === 0 ? word.toLowerCase() : '-' + word.toLowerCase())).replace(/\s+/g, '');
 
 // Get days name from a date
-export const getDayName = (date: Date, lng: string) => {
+export const getDayNameByDate = (date: Date, lng: string) => {
   const enDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const frDays = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
 
@@ -39,6 +39,27 @@ export const getDayName = (date: Date, lng: string) => {
       break;
     default:
       return frDays[date.getDay()];
+      break;
+  }
+}
+
+export const getDayNameByNumber = (id: number, lng: string) => {
+  const enDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const frDays = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+
+  if (id < 0 || id > 6) {
+    return 'Invalid day number';
+  }
+
+  switch (lng) {
+    case 'fr-FR':
+      return frDays[id];
+      break;
+    case 'en-US':
+      return enDays[id];
+      break;
+    default:
+      return frDays[id];
       break;
   }
 }
